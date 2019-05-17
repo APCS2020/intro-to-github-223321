31e00ba9777c5a6a918538ffc96a34456795ff7e
谭哲 harry <50470335+223321@users.noreply.github.com>
2019-05-17 10:45:59 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,20 @@
-public class test{
   
-  
-  
-  
-  
+  public class tanzhe{
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++){
+            for (int j = i; j < 2; i++){
+                System.out.print("*");
+            }
+                for (int h = 0; h < i + 1; h++){
+                    System.out.print("A");
+                }
+                    System.out.println();
+                }
+            }
+    }
   
   
   
   
   }
+
